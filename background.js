61703d6eb062a9641ef230e18e1465264ff87b56
Vetrila, Dorian <dorian.vetrila@sky.uk>
2016-04-20 09:09:34 +0100

    Introducing Callbacks.

--- background.js
+++ background.js
@@ -1,13 +1,1 @@
 //
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.update(audibleTabs[0].id, {selected: true});
-  chrome.windows.update(audibleTabs[0].windowId, {'focused':true});
-  arrayRotate(audibleTabs);
-});
-
-
-chrome.windows.onRemoved.addListener(function(windowID){
-  audibleTabs = audibleTabs.filter(function(tab){
-      return tab.windowId !== windowID;
-    });
-});
--- listeners.js
+++ listeners.js
@@ -1,22 +1,35 @@
 //
 chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab) {
   if(changeInfo.audible === false) {
-    audibleTabs = audibleTabs.filter(function(tab){
+    tabs.audible = tabs.audible.filter(function(tab){
       return tab.id !== tabID;
     });
   } 
   else if(changeInfo.audible === true) {
-     audibleTabs.push(tab);
+     tabs.audible.push(tab);
   }
 });
 
 //Tab ID changes when attached to a new window. Update with new value. 
 chrome.tabs.onAttached.addListener(function(tabId){
     chrome.tabs.get(tabId, function(tab) {
-      audibleTabs.forEach(function(e){
+      tabs.audible.forEach(function(e){
         if(e.id === tabId){
-          audibleTabs[audibleTabs.indexOf(e)] = tab;
+          tabs.audible[tabs.audible.indexOf(e)] = tab;
         }
       });
+    });
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.update(tabs.audible[0].id, {selected: true});
+  chrome.windows.update(tabs.audible[0].windowId, {'focused':true});
+  arrayRotate(tabs.audible);
+});
+
+
+chrome.windows.onRemoved.addListener(function(windowID){
+  tabs.audible = tabs.audible.filter(function(tab){
+      return tab.windowId !== windowID;
     });
 });
